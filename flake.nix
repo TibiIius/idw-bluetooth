470933a185b53e1b0e20ff8b87b02dd80bd739ae
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-03 10:28:51 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake file for IDW Bluetooth course project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
