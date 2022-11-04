899abf13771bd5c2e792b075510c124d05f65be0
Jecksa <76112538+Jecksa@users.noreply.github.com>
2022-11-04 06:12:47 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,33 @@
-import sys
-import pydbus
+from bluepy.btle import Scanner, DefaultDelegate
 
-if __name__ == "__main__":
-    bus = pydbus.SystemBus()
+##Scan Area for all Devices
+class ScanDelegate(DefaultDelegate):
+    def __init__(self)
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, scanEntry, isNewDev, isNewData)
+		if isNewData:
+			print ('Discovered device", scanEntry.addr)
+		elif isNewData:
+			print ('Recieved new data from", scanEntry.addr)
+       
+ 	scanner = Scanner.withDelegate(ScanDelegate())
+	devices = scanner.scan(10.0)
+
+	for dev in devices:
+		print('Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+		for(adtype, desc, value) in dev.getScanData():
+			print('%s = %s' % (desc, value))
+
+##Scan only for known device
+##class ScanDelegate(DefaultDelegate):
+##    def __init__(self)
+##        DefaultDelegate.__init__(self)
+##              
+##while True:
+##	scanner = Scanner.withDelegate(ScanDelegate())
+##	devices = scanner.scan(10.0)
+##
+##	for dev in devices:
+##		if(dev.addr = ''):
+##			print('Device %s, RSSI=%d dB" % (dev.addr, dev.rssi)
