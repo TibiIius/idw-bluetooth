6bb7f1d5bc17936639ecc7fb8fef057e175e31e6
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-04 10:22:49 +0100

    make stuff

--- src/main.py
+++ src/main.py
@@ -2,10 +2,20 @@ import pydbus
 import time
 import atexit
 
+# This is the RSSI value at 1 meter distance
+MEASURED_POWER = -48
+
+
+def get_distance(rssi):
+    # Calculate distance from RSSI value
+    return 10 ** ((MEASURED_POWER - rssi) / (10 * 2))
+
+
 if __name__ == "__main__":
     bus = pydbus.SystemBus()
     host = bus.get("org.bluez", "/org/bluez/hci0")
-    dev = bus.get("org.bluez", "/org/bluez/hci0/dev_20_9B_A5_9A_B5_0D")
+    # dev = bus.get("org.bluez", "/org/bluez/hci0/dev_20_9B_A5_9A_B5_0D")
+    dev = bus.get("org.bluez", "/org/bluez/hci0/dev_F0_79_60_4A_D6_85")
 
     # We connect a hook on exit here so we can reconnect the device before shutdown
     atexit.register(lambda: dev.Connect())
@@ -22,4 +32,5 @@ if __name__ == "__main__":
 
     while True:
         print(dev.Get("org.bluez.Device1", "RSSI"))
+        print(get_distance(dev.Get("org.bluez.Device1", "RSSI")))
         time.sleep(1)
